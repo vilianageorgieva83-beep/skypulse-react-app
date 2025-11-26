1ccefaa61d37f3166d79b969335700fd372c71a7
ViliG <viliana.georgieva83@gmail.com>
2025-11-26 08:47:48 +0100

    update Layout to include Hero and Footer and Home-based conditional rendering

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,13 +1,23 @@
 import Navbar from "../components/Navbar";
-import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import { Outlet, useLocation } from "react-router-dom";
 
 export default function Layout() {
+  const location = useLocation();
+  const isHome = location.pathname === "/";
+
   return (
-    <div className="min-h-screen bg-blue-50 text-gray-900">
+    <div className="min-h-screen flex flex-col bg-blue-50 text-gray-900">
       <Navbar />
-      <main className="p-4 max-w-5xl mx-auto">
+
+      {isHome && <Hero />}
+
+      <main className="p-4 max-w-5xl mx-auto flex-1">
         <Outlet />
       </main>
+
+      <Footer />
     </div>
   );
 }
